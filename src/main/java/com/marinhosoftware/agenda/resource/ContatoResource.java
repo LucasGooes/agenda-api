1a22d55de2702a29d5fc41ccdbf2282bc1a7db3c
LucasGooes <lucasmarcos343@gmail.com>
2021-04-29 17:33:57 -0400

    implementando POST, DELETE e Get de Contato

--- src/main/java/com/marinhosoftware/agenda/resource/ContatoResource.java
+++ src/main/java/com/marinhosoftware/agenda/resource/ContatoResource.java
@@ -1,11 +1,16 @@
 package com.marinhosoftware.agenda.resource;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marinhosoftware.agenda.domain.Contato;
 import com.marinhosoftware.agenda.service.ContatoService;
@@ -22,6 +27,26 @@ public class ContatoResource {
 		Contato obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Contato obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Contato> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Contato>> findAll() {
+		List<Contato> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
 
 }
--- src/main/java/com/marinhosoftware/agenda/service/ContatoService.java
+++ src/main/java/com/marinhosoftware/agenda/service/ContatoService.java
@@ -1,12 +1,15 @@
 package com.marinhosoftware.agenda.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marinhosoftware.agenda.domain.Contato;
 import com.marinhosoftware.agenda.repository.ContatoRepository;
+import com.marinhosoftware.agenda.service.exception.ObjectNotFoundException;
 
 @Service
 public class ContatoService {
@@ -16,7 +19,23 @@ public class ContatoService {
 	
 	public Contato find(Integer id) {
 		Optional<Contato> obj = repo.findById(id);
-		return obj.orElseThrow();
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Contato.class.getName()));
+	}
+	
+	@Transactional
+	public Contato insert(Contato obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
+	
+	public List<Contato> findAll() {
+		return repo.findAll();
 	}
 
 }
